abed7a67b603b95432e6302629967688e7ea8659
Luis Augusto <hello@luis.app>
2021-04-09 18:41:48 -0700

    Setup function to get all new posts within the last half hour

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
     "import",
   ],
   rules: {
-    quotes: ["error", "double"],
+    "quotes": ["error", "double"],
+    "max-len": [1, 120],
   },
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,64 @@
 import * as functions from "firebase-functions";
+import axios from "axios";
 
-export const getArticles = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+type Article = {
+    // eslint-disable-next-line camelcase
+    published_timestamp: string
+    id: number
+}
+
+type User = {
+    username: string
+}
+
+export const getArticles = functions.https.onRequest(async (request, response) => {
+  const api = "https://dev.to/api";
+
+  const lastHour = (article: Article): boolean => {
+    // This checks if the article's publish date is within one hour of the current date
+
+    const articleDate = new Date(article.published_timestamp);
+    const dateDifference = (new Date()).getTime() - articleDate.getTime();
+
+    // Converts timestamp from milliseconds to hours
+    // @todo change 1000 hours to compare to 1 hour
+    return dateDifference / 1000 / 60 / 60 < 1000;
+  };
+
+  const {data: orgArticles}: {data: Article[]} = await axios.get(`${api}/organizations/the-collab-lab/articles`);
+  const newOrgArticles = orgArticles.filter(lastHour);
+
+  const {data: users}: {data: User[]} = await axios.get(`${api}/organizations/the-collab-lab/users?per_page=1000`);
+  let allNewUserArticles: Article[] = [];
+
+  const fetchUser = async (user: User) => {
+    const {data: userArticles}: {data: Article[]} = await axios.get(`${api}/articles/latest?username=${user.username}`);
+
+    const newUserArticles = userArticles.filter(lastHour).filter((article) => (
+      // Filter out the articles that are already included in the organization pull
+      !newOrgArticles.some((orgArticle) => article.id === orgArticle.id)
+    ));
+
+    functions.logger.info(`Articles for: ${user.username} (${newUserArticles.length})`, newUserArticles);
+
+    allNewUserArticles = [...allNewUserArticles, ...newUserArticles];
+  };
+
+  const delay = (milliseconds: number) => new Promise((resolve) => setTimeout(resolve, milliseconds));
+
+  const userPromises = users.map(async (user, index) => {
+    // Stagger fetch request, limit for dev.to is 3 requests per second
+    await delay(1500 * index);
+    await fetchUser(user);
+  });
+  await Promise.all(userPromises);
+
+  const allNewArticles = [...newOrgArticles, ...allNewUserArticles];
+
+  functions.logger.info("Organization articles found:", newOrgArticles);
+  functions.logger.info("Number of organization articles found:", newOrgArticles.length);
+  functions.logger.info("Member articles found:", allNewUserArticles);
+  functions.logger.info("Number of member articles found:", allNewUserArticles.length);
+  functions.logger.info("Number of new articles found:", allNewArticles.length);
+  response.json({data: allNewArticles});
 });
